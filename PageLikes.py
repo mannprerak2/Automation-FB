f67a005a56d2114cc127d36ece67eb0815722d6a
mannprerak2 <31154435+mannprerak2@users.noreply.github.com>
2018-01-16 18:43:01 +0530

    Add files via upload
    initial commit

---
+++ PageLikes.py
@@ -0,0 +1,72 @@
+# coding: utf-8
+
+#PreRequisites-
+    #Be sure to change chrome driver path in get driver function (line 37)
+    #use python3
+    #install selenium for python3
+    
+# structure of this file-----
+    # 1) imports
+    # 2) gloabal variables
+    # 3) functions
+    # 4) code
+    # 5) algo or how to...(complex stuff only)
+
+
+#imports------start
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import getpass
+#inports-------end
+
+
+#global variables----------start
+email_str = ""
+pass_str = ""
+driver = webdriver
+#global variables----------end
+
+
+#functions---------start
+def askLoginDetails():
+    global email_str,pass_str
+    email_str = raw_input("Enter fb email > ")
+    pass_str = getpass.getpass("Enter password >")
+
+def getDriver():
+    global driver
+    driver = webdriver.Chrome(
+    executable_path="/home/prerak/AnacondaProjects/chromedriver")
+    #change this acc to driver location in ur pc
+
+def openFB():
+    driver.get("https://www.facebook.com/")
+
+def loginToFB():
+    emailbox = driver.find_element_by_id("email")
+    emailbox.send_keys(email_str)#enter email
+    passbox = driver.find_element_by_id("pass")
+    passbox.send_keys(pass_str)#enter password
+    passbox.send_keys(Keys.RETURN)
+#functions---------end
+
+
+#code logic-------start
+askLoginDetails()
+getDriver()
+openFB()
+loginToFB()
+#code logic-------end
+
+
+
+#algo --------start
+'''
+
+
+
+
+'''
+#algo---------end
