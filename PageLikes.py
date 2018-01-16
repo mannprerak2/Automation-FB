6be35d90c8cb9d94c2524a8385b4680177436395
saurabhmittal16 <saurabhmittal1606@gmail.com>
2018-01-16 19:15:13 +0530

    Change selection method of elements

--- PageLikes.py
+++ PageLikes.py
@@ -32,22 +32,23 @@ driver = webdriver
 #functions---------start
 def askLoginDetails():
     global email_str,pass_str
-    email_str = raw_input("Enter fb email > ")
+    email_str = input("Enter fb email > ")
     pass_str = getpass.getpass("Enter password >")
 
 def getDriver():
     global driver
     driver = webdriver.Chrome(
-    executable_path="/home/prerak/AnacondaProjects/chromedriver")
+        # executable_path="/home/prerak/AnacondaProjects/chromedriver") - Prerak
+        # executable_path="/usr/bin/chromedriver") - Saurabh
     #change this acc to driver location in ur pc
 
 def openFB():
     driver.get("https://www.facebook.com/")
 
 def loginToFB():
-    emailbox = driver.find_element_by_id("email")
+    emailbox = driver.find_element_by_css_selector("input[type='email']")
     emailbox.send_keys(email_str)#enter email
-    passbox = driver.find_element_by_id("pass")
+    passbox = driver.find_element_by_css_selector("input[type='password']")
     passbox.send_keys(pass_str)#enter password
     passbox.send_keys(Keys.RETURN)
 #functions---------end
