4db7db414c984e6f80de83f4d98ae6457f07da54
Amandeep Singh <a.singh.2313@gmail.com>
2018-01-16 22:16:24 +0530

    m

--- PageLikes.py
+++ PageLikes.py
@@ -43,10 +43,9 @@ def getDriver():
     global driver
     chrome_options=webdriver.ChromeOptions()
     chrome_options.add_argument("--incognito")
-    driver = webdriver.Chrome(
-        # executable_path="/home/prerak/AnacondaProjects/chromedriver",chrome_options=chrome_options) - Prerak
-        # executable_path='/Users/Batman/anaconda/chromedriver',chrome_options=chrome_options) - Aman
-        # executable_path="/usr/bin/chromedriver",chrome_options=chrome_options) - Saurabh
+    #driver = webdriver.Chrome(executable_path='/Users/Batman/anaconda3/chromedriver',chrome_options=chrome_options)#amandeep
+    driver = webdriver.Chrome(executable_path="/home/prerak/AnacondaProjects/chromedriver",chrome_options=chrome_options)#prerak
+    #driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver",chrome_options=chrome_options) #saurabh
     #change this acc to driver location in ur pc
 
 def openFB():
@@ -60,17 +59,23 @@ def loginToFB():
     passbox.send_keys(Keys.RETURN)
 
 def visitPage():
-    driver.get("https://www.facebook.com/pg/YourEdm/posts/")
+    pageName = "https://www.facebook.com/pg/YourEdm/posts/"
+    driver.get(pageName)
+    print("Visiting ->",pageName) 
 
 def likePost():
-    cancel = driver.find_element_by_css_selector("a[action='cancel']")  #manually exiting the notification pop-up
-    cancel.click()
-    
+    #cancel = driver.find_element_by_css_selector("a[action='cancel']")  #manually exiting the notification pop-up
+    #cancel.click()  # not required as we use icognito 
+    counter = 0
     like = driver.find_elements_by_css_selector("a[role='button'][href='#'][tabindex='0']")
-    time.sleep(3)  
+    time.sleep(3)
+    print("Total buttons found ->",len(like))  
     for i in range(len(like)):
         if (like[i].get_attribute('text') == 'Like'):
             like[i].click()
+            counter+=1
+    print ("Number of posts liked ->",counter)
+    
 #functions---------end
 
 
