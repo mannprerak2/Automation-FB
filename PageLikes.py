6abfc4bc16d81ec1ccbe0e6eaa82dca80aff26b2
saurabhmittal16 <saurabhmittal1606@gmail.com>
2018-01-18 17:50:22 +0530

    Basic debugging

--- PageLikes.py
+++ PageLikes.py
@@ -3,10 +3,10 @@
 
 #PreRequisites-
     #Be sure to change chrome driver path in get driver function (line 37)
-    #use python3
-    #install selenium for python3
+    #Use python3
+    #Install selenium for python3
     
-# structure of this file-----
+# Structure of this file-----
     # 1) imports
     # 2) gloabal variables
     # 3) functions
@@ -25,28 +25,28 @@ import getpass
 #imports-------end
 
 
-
 #global variables----------start
 email_str = ""
 pass_str = ""
+pageName = ""
 driver = webdriver
 #global variables----------end
-
 
 
 #functions---------start
 def askLoginDetails():
     global email_str,pass_str
-    email_str = input("Enter facebook e-mail > ")
+    email_str = input("Enter Facebook e-mail or username > ")
     pass_str = getpass.getpass("Enter password >")    
 
 def getDriver():
     global driver
     chrome_options=webdriver.ChromeOptions()
     chrome_options.add_argument("--incognito")
-    #driver = webdriver.Chrome(executable_path='/Users/Batman/anaconda3/chromedriver',chrome_options=chrome_options)#amandeep
-    # driver = webdriver.Chrome(executable_path="/home/prerak/AnacondaProjects/chromedriver",chrome_options=chrome_options)#prerak
-    driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver",chrome_options=chrome_options) #saurabh
+    #driver = webdriver.Chrome(executable_path='/Users/Batman/anaconda3/chromedriver',chrome_options=chrome_options)            #amandeep
+    # driver = webdriver.Chrome(executable_path="/home/prerak/AnacondaProjects/chromedriver",chrome_options=chrome_options)     #prerak
+    driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver",chrome_options=chrome_options)                            #saurabh
+    
     #change this acc to driver location in ur pc
 
 def openFB():
@@ -54,10 +54,11 @@ def openFB():
 
 def loginToFB():
     global email_str,pass_str
+
     emailbox = driver.find_element_by_css_selector("input[type='email']")
-    emailbox.send_keys(email_str)#enter email
+    emailbox.send_keys(email_str)                                            #enter email
     passbox = driver.find_element_by_css_selector("input[type='password']")
-    passbox.send_keys(pass_str)#enter password
+    passbox.send_keys(pass_str)                                              #enter password
     passbox.send_keys(Keys.RETURN)
 
 def check_id():
@@ -76,7 +77,10 @@ def login():
         check_id()
         
 def visitPage():
-    pageName = "https://www.facebook.com/pg/YourEdm/posts/"
+    global pageName
+
+    pageName = input("Enter the page link >")
+    pageName = pageName + 'posts'
     print("Visiting ->",pageName) 
     driver.get(pageName)
 
@@ -84,6 +88,7 @@ def visitPage():
 def likePost():
     #cancel = driver.find_element_by_css_selector("a[action='cancel']")  #manually exiting the notification pop-up
     #cancel.click()  # not required as we use icognito 
+    
     counter = 0
     last_height = 0
     i=0
@@ -94,20 +99,20 @@ def likePost():
         like = driver.find_elements_by_css_selector("a[data-testid='fb-ufi-likelink'][tabindex='0']")
         for index in range(len(like)):
             if (like[index].is_displayed()):
-                if (like[index].get_attribute('clientWidth') > 0):
-                    like[index].click()
+                if (int(like[index].get_attribute('clientWidth')) > 0):
+                    driver.execute_script("arguments[0].click();",like[index])
+                    counter+=1
         new_height = driver.execute_script("return document.body.scrollHeight")
         
         if new_height == last_height:
             break
-        
         last_height = new_height
         i+=1
-        
         if (i>10):
-            break
-#functions---------end
+           break
+    print (str(counter) + " posts liked")
 
+#functions---------end
 
 
 #code logic-------start
